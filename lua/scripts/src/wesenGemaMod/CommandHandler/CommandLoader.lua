5dc0dc88a63b8dde74f54ce97430e1ed176fb8ee
wesen1 <wesen1@users.noreply.github.com>
2018-03-09 23:24:11 +0100

    Add documentation for CommandLoader and CommandParser

--- lua/scripts/src/wesenGemaMod/CommandHandler/CommandLoader.lua
+++ lua/scripts/src/wesenGemaMod/CommandHandler/CommandLoader.lua
@@ -1,54 +1,68 @@
 ---
 -- @author wesen
 -- @copyright 2018 wesen <wesen-ac@web.de>
--- 
+-- @release 0.1
+-- @license MIT
+--
 
 ---
--- Automatically loads all commands.
+-- @type CommandLoader Loads all commands from the Commands directory.
 --
 local CommandLoader = {};
 
 
+---
+-- The parent command handler
+--
+-- @tfield CommandHandler parentCommandHandler
+--
 CommandLoader.parentCommandHandler = "";
 
 
 ---
 -- CommandLoader constructor.
--- 
+--
+-- @tparam CommandHandler _parentCommandHandler The parent command handler
+--
+-- @treturn CommandLoader The CommandLoader instance
+--
 function CommandLoader:__construct(_parentCommandHandler)
 
   local instance = {};
   setmetatable(instance, {__index = CommandLoader});
-  
+
   instance.parentCommandHandler = _parentCommandHandler;
-  
+
   return instance;
 
 end
 
 
+-- Class Methods
+
+---
+-- Loads all commands from the Commands directory and adds them to _commandLister.
+--
+-- @tparam CommandLister _commandLister The command lister
+--
 function CommandLoader:loadCommands(_commandLister)
-  
-  local lfs = require("lfs"); 
 
-  -- iterate over each file in the Commands directory 
-  for luaFile in lfs.dir("lua/scripts/src/wesenGemaMod/Commands") do 
- 
-    if (luaFile ~= "." and luaFile ~= "..") then
-    
-      -- Don't load the base command since its just a structure for the real commands
-      if (luaFile ~= "BaseCommand.lua") then
+  local lfs = require("lfs");
 
-        local command = require("Commands/" .. luaFile:gsub(".lua", ""));
-        
-        _commandLister:addCommand(command);   
-        print("Loaded command !" .. luaFile:gsub(".lua", ""));
-        
-      end
- 
-    end 
- 
-  end 
+  -- iterate over each file in the Commands directory
+  for luaFile in lfs.dir("lua/scripts/src/wesenGemaMod/Commands") do
+
+    if (luaFile ~= "." and luaFile ~= ".." and luaFile ~= "BaseCommand.lua") then
+
+      local commandClassName = luaFile:gsub(".lua", "");
+      local command = require("Commands/" .. commandClassName);
+
+      _commandLister:addCommand(command);
+      print("Loaded command !" .. commandClassName);
+
+    end
+
+  end
 
 end
 
--- lua/scripts/src/wesenGemaMod/CommandHandler/CommandParser.lua
+++ lua/scripts/src/wesenGemaMod/CommandHandler/CommandParser.lua
@@ -1,99 +1,121 @@
 ---
 -- @author wesen
--- @copyright 2017 wesen <wesen-ac@web.de>
--- 
+-- @copyright 2017-2018 wesen <wesen-ac@web.de>
+-- @release 0.1
+-- @license MIT
+--
 
 local Output = require("Outputs/Output");
 local StringUtils = require("Utils/StringUtils");
 local TableUtils = require("Utils/TableUtils");
 
 ---
--- Handles command parsing and execution.
+-- @type CommandParser Handles command parsing and execution.
 --
 local CommandParser = {};
 
 
+---
+-- The parent command handler
+--
+-- @tfield CommandHandler parentCommandHandler
+--
 CommandParser.parentCommandHandler = "";
+
 
 ---
 -- CommandParser constructor.
--- 
+--
+-- @tparam CommandHandler _parentCommandHandler The parent command handler
+--
+-- @treturn CommandParser The CommandParser instance
+--
 function CommandParser:__construct(_parentCommandHandler)
 
   local instance = {};
   setmetatable(instance, {__index = CommandParser});
-  
+
   instance.parentCommandHandler = _parentCommandHandler;
-  
+
   return instance;
 
 end
 
 
+---
+-- Checks whether a string starts with "!" followed by other characters than "!".
 --
--- Checks whether a string is starting with "!" followed by other characters than !.
+-- @tparam string _text The string
 --
--- @param string _commandName   The string which will be checked
---
--- @return bool       True: The string is a command
---                    False: The string is not a command
+-- @treturn bool True: The string is a command
+--               False: The string is not a command
 --
 function CommandParser:isCommand(_text)
-  
+
   -- if first character is "!" and second is not "!"
   if string.sub(_text,1,1) == "!" and string.sub(_text,2,2) ~= "!" then
     return true;
   else
     return false;
-  end  
-  
+  end
+
 end
 
+---
+-- Splits an input text into command name and arguments and executes the command if it exists.
 --
--- Splits the input into command name and arguments and tries to execute the command.
+-- @tparam string _inputText The input text in the format "!commandName args"
+-- @tparam int _cn The client number of the player who tries to execute the command
 --
--- @param String _text  Input text in the format "!commandName args"
--- @param int _cn        Client number of the player who tries to execute the command
---
-function CommandParser:parseCommand(_text, _cn)
+function CommandParser:parseCommand(_inputText, _cn)
 
-  local parts = StringUtils:split(_text, " ");
-  local commandName = string.lower(parts[1]);
-  
-  local args = {};
-  if (#parts > 1) then
-    args = TableUtils:slice(parts, 2);
-  end
-  
+  local inputTextParts = StringUtils:split(_inputText, " ");
+  local commandName = string.lower(inputTextParts[1]);
   local command = self.parentCommandHandler:getCommand(commandName);
-      
+
   if (command) then
-  
-    local player = self.parentCommandHandler:getParentGemaMod():getPlayers()[_cn];
-         
-    if (player:getLevel() >= command:getRequiredLevel()) then
-        
-      if (#args < command:getNumberOfRequiredArguments()) then
-        Output:print(Output:getColor("error") .. "Error: Not enough arguments.");
-      
-      elseif (#args > command:getNumberOfArguments()) then
-        Output:print(Output:getColor("error") .. "Error: Too many arguments");
-        
-      else
-        command:execute(_cn, args);
-      
-      end
-      
-    else
-      Output:print(Output:getColor("error") .. "Error: You do not have the permission to use this command!", _cn);
-    end 
-      
-  else   
-    
+
+    local arguments = {};
+    if (#inputTextParts > 1) then
+      arguments = TableUtils:slice(inputTextParts, 2);
+    end
+
+    self:executeCommand(command, arguments, _cn);
+
+  else
     Output:print(Output:getColor("error") .. "Unknown command '" .. commandName .. "', check your spelling and try again", _cn);
-  
   end
-  
+
+end
+
+---
+-- Executes a command.
+--
+-- @tparam BaseCommand _command The command
+-- @tparam string[] _arguments The arguments
+-- @tparam int The client number of the player who tries to execute the command
+--
+function CommandParser:executeCommand(_command, _arguments, _cn)
+
+  local player = self.parentCommandHandler:getParentGemaMod():getPlayers()[_cn];
+
+  if (player:getLevel() >= _command:getRequiredLevel()) then
+
+    if (#_arguments < _command:getNumberOfRequiredArguments()) then
+      Output:print(Output:getColor("error") .. "Error: Not enough arguments.");
+
+    elseif (#_arguments > _command:getNumberOfArguments()) then
+      Output:print(Output:getColor("error") .. "Error: Too many arguments");
+
+    else
+      _command:execute(_cn, _arguments);
+
+    end
+
+  else
+    Output:print(Output:getColor("error") .. "Error: You do not have the permission to use this command!", _cn);
+  end
+
 end
 
 
