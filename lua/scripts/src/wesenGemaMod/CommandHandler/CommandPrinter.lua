db116290da4eb2858c41030f777d7e99c164b5c8
wesen1 <wesen1@users.noreply.github.com>
2018-03-10 00:04:34 +0100

    Add documentation for CommandPrinter

--- lua/scripts/src/wesenGemaMod/CommandHandler/CommandPrinter.lua
+++ lua/scripts/src/wesenGemaMod/CommandHandler/CommandPrinter.lua
@@ -1,30 +1,41 @@
 ---
 -- @author wesen
--- @copyright 2017 wesen <wesen-ac@web.de>
--- 
+-- @copyright 2017-2018 wesen <wesen-ac@web.de>
+-- @release 0.1
+-- @license MIT
+--
 
 local Output = require("Outputs/Output");
 local TableOutput = require("Outputs/TableOutput");
 
 ---
--- Handles printing of commands in !cmds and !help.
--- 
+-- @type CommandPrinter Handles printing of commands in !cmds and !help.
+--
 local CommandPrinter = {};
 
 
+---
+-- The parent command handler
+--
+-- @tfield CommandHandler parentCommandHandler
+--
 CommandPrinter.parentCommandHandler = "";
 
 
 ---
 -- CommandPrinter constructor.
--- 
+--
+-- @tparam CommandHandler _parentCommandHandler The parent command handler
+--
+-- @treturn CommandPrinter The CommandPrinter instance
+--
 function CommandPrinter:__construct(_parentCommandHandler)
 
   local instance = {};
   setmetatable(instance, {__index = CommandPrinter});
-  
+
   instance.parentCommandHandler = _parentCommandHandler;
-  
+
   return instance;
 
 end
@@ -32,21 +43,24 @@ end
 
 ---
 -- Generates and returns a command string.
--- 
--- @param _command (BaseCommand) The command
--- @param _showOnlyRequiredArguments (bool) Indicates whether only required arguments will be added to the command string
--- 
+--
+-- @tparam BaseCommand _command The command
+-- @tparam bool _showOnlyRequiredArguments True: Only required arguments will be added to the command string
+--                                         False: All arguments will be added to the command string
+--
+-- @treturn string The command string
+--
 function CommandPrinter:generateCommandString(_command, _showOnlyRequiredArguments)
 
   local commandString = Output:getColor("command" .. _command:getRequiredLevel())
                      .. "!" .. _command:getName();
-  
+
   for i, argument in ipairs(_command:getArguments()) do
 
     if (not argument["isRequired"] and not _showOnlyRequiredArguments) then
       commandString = commandString
-                     .. Output:getColor("optionalArgument")
-                     .. " <" .. argument["name"] .. ">";
+                   .. Output:getColor("optionalArgument")
+                   .. " <" .. argument["name"] .. ">";
 
     elseif (argument["isRequired"]) then
       commandString = commandString
@@ -62,81 +76,82 @@ end
 
 ---
 -- Returns an output list for all arguments of a command.
--- 
--- @param _command (BaseCommand) The command
--- 
--- @return (table) Argument output list
+--
+-- @tparam BaseCommand _command The command
+--
+-- @treturn string[] The argument output list
 --
 function CommandPrinter:getArgumentOutputList(_command)
-  
+
   local arguments = {};
-          
+
   for i, argument in ipairs(_command:getArguments()) do
 
     local argumentName = "<" .. argument["name"] .. ">";
-    local argumentDescription = argument["description"];
+    local argumentDescription = Output:getColor("helpDescription") .. ": " .. argument["description"];
 
-    if (argument["isRequired"]) then 
+    -- Set the output color for the argument name
+    if (argument["isRequired"]) then
       argumentName = Output:getColor("requiredArgument") .. argumentName;
     else
       argumentName = Output:getColor("optionalArgument") .. argumentName;
       argumentDescription = argumentDescription .. " (Optional)";
     end
-    
-    local prefix = "  ";
+
+    -- Determine the prefix of the argumentName
     if (i == 1) then
-      prefix = ": ";
+      -- The first argument is in the same line like the title "Arguments", so it must contain a colon
+      argumentName = ": " .. argumentName;
+    else
+      -- All other lines may not contain colons
+      argumentName = "  " .. argumentName;
     end
-    
-    table.insert(arguments, {
-      prefix .. argumentName,
-      Output:getColor("helpDescription") .. ": " .. argumentDescription
-    });
+
+    table.insert(arguments, {argumentName, argumentDescription});
 
   end
-  
+
   return arguments;
 
 end
 
 ---
 -- Prints the list of available commands to a player.
--- 
--- @param _cn (int) Player cn
+--
+-- @tparam int _cn The client number of the player
 --
 function CommandPrinter:printCommandList(_cn, _commandLister)
 
   local player = self.parentCommandHandler:getParentGemaMod():getPlayers()[_cn];
 
   Output:print(Output:getColor("cmdsTitle") .. "Available commands:", _cn);
-  
+
   local rows = {};
-  
+
   local groupedCommands = _commandLister:getGroupedCommands();
-  
-  for index, level in pairs(_commandLister:getSortedCommandLevels()) do
-  
+
+  for index, level in ipairs(_commandLister:getSortedCommandLevels()) do
+
     if (player:getLevel() < level) then
       break;
     end
 
     local commandGroups = groupedCommands[level];
 
-    for level, groupName in pairs(_commandLister:getSortedCommandGroupNames()[level]) do
+    for level, groupName in ipairs(_commandLister:getSortedCommandGroupNames()[level]) do
 
       local groupCommandString = "";
 
-      for index, commandName in pairs(commandGroups[groupName]) do
-
+      for index, commandName in ipairs(commandGroups[groupName]) do
         local command = _commandLister:getCommand(commandName);
         groupCommandString = groupCommandString .. self:generateCommandString(command, true) .. "   ";
       end
-        
+
       local row = {
         " " .. groupName,
         ": " .. groupCommandString
       }
-  
+
       table.insert(rows, row);
 
     end
@@ -149,30 +164,30 @@ end
 
 ---
 -- Prints the help text for a command.
--- 
--- @param _command (BaseCommand) The command
--- @param _cn (int) Player cn to which the help text will be printed
--- 
+--
+-- @tparam BaseCommand _command The command
+-- @tparam int _cn The client number of the player to which the help text will be printed
+--
 function CommandPrinter:printHelpText(_command, _cn)
-    
+
     local rows = {
       [1] = {
         Output:getColor("helpTitle") .. "Usage ",
         ": " .. self:generateCommandString(_command, false)
       },
-      [2] = { 
+      [2] = {
         Output:getColor("helpTitle") .. "Description ",
         ": " .. Output:getColor("helpDescription") .. _command:getDescription()
       }
-    }    
-    
+    }
+
     if (_command:getNumberOfArguments() > 0) then
 
       rows[3] = {
         Output:getColor("helpTitle") .. "Arguments",
         self:getArgumentOutputList(_command)
       }
-      
+
     end
 
     TableOutput:printTable(rows, _cn);
