84e39dfdaa3e8fe56b70610c3412d9fba72533da
wesen1 <wesen1@users.noreply.github.com>
2018-03-10 11:43:29 +0100

    Add documentation for main.lua

--- lua/scripts/main.lua
+++ lua/scripts/main.lua
@@ -1,12 +1,14 @@
 ---
 -- @author wesen
 -- @copyright 2017-2018 wesen <wesen-ac@web.de>
--- 
-PLUGIN_NAME = "wesen's basic gema mod";
+-- @release 0.1
+-- @license MIT
+--
+PLUGIN_NAME = "wesen's gema mod";
 PLUGIN_AUTHOR = "wesen";
 PLUGIN_VERSION = 0.1;
 
--- 
+--
 -- Add the path to the wesenGemaMod classes to the package path list
 -- in order to be able to omit this path portion in require() calls
 --
