a5f6625365836abd81d9c16a066544377107c0ef
wesen1 <wesen1@users.noreply.github.com>
2018-03-17 21:36:48 +0100

    Add example server configuration

--- VagrantProvision.sh
+++ VagrantProvision.sh
@@ -11,3 +11,11 @@ END
 
 # Create a link to the lua folder of the project in the lua_server folder
 ln -fs /vagrant/lua /home/vagrant/lua_server
+
+rm -rf /home/vagrant/lua_server/config
+ln -fs /vagrant/test\ server/config /home/vagrant/lua_server/config
+
+rm -rf /home/vagrant/lua_server/packages/maps/official
+rm -rf /home/vagrant/lua_server/packages/maps/servermaps/incoming
+ln -fs /vagrant/test\ server/maps /home/vagrant/lua_server/packages/maps/servermaps/incoming
+
