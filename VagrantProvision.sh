3f97c2fc93a02cf715219b31c3ea650df366a6fc
wesen1 <wesen1@users.noreply.github.com>
2018-04-02 16:45:55 +0200

    Add server logs to test server folder

--- VagrantProvision.sh
+++ VagrantProvision.sh
@@ -21,3 +21,11 @@ ln -fs /vagrant/test\ server/config /home/vagrant/lua_server/config
 
 rm -rf /home/vagrant/lua_server/packages/maps/servermaps/incoming
 ln -fs /vagrant/test\ server/maps /home/vagrant/lua_server/packages/maps/servermaps/incoming
+
+# Redirect logs to logfile
+mkdir -p /vagrant/test\ server/log
+ln -fs /vagrant/test\ server/log /var/log/assaultcube
+
+echo -e "\n\n# Custom Rules" >> /etc/rsyslog.conf
+echo ":programname, isequal, AssaultCube /var/log/assaultcube/assaultcube_server.log" >> /etc/rsyslog.conf
+/etc/init.d/rsyslog restart
