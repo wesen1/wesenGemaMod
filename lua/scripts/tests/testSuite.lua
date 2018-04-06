0cf2a1f0b3b6aabcd3682ea35d9b7291b06424b0
wesen1 <wesen1@users.noreply.github.com>
2018-04-07 01:21:09 +0200

    Add unit test for TableUtils
    * Added unit test for TableUtils
    * Fixed test suite

--- lua/scripts/tests/testSuite.lua
+++ lua/scripts/tests/testSuite.lua
@@ -13,14 +13,35 @@ require("luacov");
 local luaunit = require("luaunit");
 
 
--- Require all files in the tests/wesenGemaMod directory
-for luaFile in lfs.dir(lfs.currentdir() .. "/wesenGemaMod") do
+function requireTests(_testDirectoryPath)
+  
+  for luaFileName in lfs.dir(_testDirectoryPath) do
 
-  if (luaFile ~= "." and luaFile ~= "..") then
-    require(luaFile:gsub(".lua", ""));
+    if (luaFileName ~= "." and luaFileName ~= "..") then
+
+      local luaFilePath = _testDirectoryPath .. "/" .. luaFileName;
+      local attr = lfs.attributes(luaFilePath);
+
+      if (attr.mode == "directory") then
+        requireTests(luaFilePath);
+      else
+        
+        if not (package.path:find(_testDirectoryPath)) then
+          package.path = package.path .. ";" .. _testDirectoryPath .. "/?.lua";
+        end
+        
+        require(luaFileName:gsub(".lua", ""));
+      end
+    
+    end
+
   end
 
 end
+
+
+-- Require all files in the tests/wesenGemaMod directory
+requireTests(lfs.currentdir() .. "/wesenGemaMod");
 
 -- Run the tests
 os.exit( luaunit.LuaUnit:run() )
