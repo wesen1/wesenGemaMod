6e86d17895ba5643fd410999c361ea7034189a61
wesen1 <wesen1@users.noreply.github.com>
2018-04-06 23:58:21 +0200

    Fix test suite and add .travis.yml

--- lua/scripts/tests/testSuite.lua
+++ lua/scripts/tests/testSuite.lua
@@ -1,26 +1,26 @@
 ---
 -- The lua unit test suite
 --
-package.path = package.path .. ";./../../scripts/src/wesenGemaMod/?.lua";
+local lfs = require("lfs");
+
+package.path = package.path .. ";" .. lfs.currentdir() .. "/../src/wesenGemaMod/?.lua"
+                            .. ";" .. lfs.currentdir() .. "/wesenGemaMod/?.lua";
 
 
 -- Require luacov to get coverage information about the tests
 require("luacov");
 
 local luaunit = require("luaunit");
-local lfs = require("lfs");
 
 
 -- Require all files in the tests/wesenGemaMod directory
-for luaFile in lfs.dir("./wesenGemaMod") do
+for luaFile in lfs.dir(lfs.currentdir() .. "/wesenGemaMod") do
 
   if (luaFile ~= "." and luaFile ~= "..") then
-    
-    local includePath = "./wesenGemaMod/" .. luaFile:gsub(".lua", "");
-    require(includePath);
+    require(luaFile:gsub(".lua", ""));
   end
 
 end
 
 -- Run the tests
-os.exit( M.LuaUnit:run() )
+os.exit( luaunit.LuaUnit:run() )
