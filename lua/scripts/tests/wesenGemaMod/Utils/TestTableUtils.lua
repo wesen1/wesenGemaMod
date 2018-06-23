2044d39ad618d2f6e92bf3978b924bc15b87fba4
wesen <wesen1@users.noreply.github.com>
2018-06-23 15:50:08 +0200

    Revert "Make unit test fail"
    This reverts commit b3e246dca75c2aedd9878f95132adcd23049530f.

--- lua/scripts/tests/wesenGemaMod/Utils/TestTableUtils.lua
+++ lua/scripts/tests/wesenGemaMod/Utils/TestTableUtils.lua
@@ -155,7 +155,7 @@ function TestTableUtils:testCanCopyTable()
   luaunit.assertEquals(testTable, testTableReference);
   luaunit.assertEquals(tostring(testTable), tostring(testTableReference));
 
-  testTableReference["name"] = "notgma";
+  testTableReference["name"] = "notgema";
   testTableReference["rank"] = 999;
   testTableReference["points"] = 0;
 
