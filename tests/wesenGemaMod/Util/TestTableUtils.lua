dcf799013212ad8082a11bfab79682c93f1793d7
wesen <wesen1@users.noreply.github.com>
2020-01-02 23:54:03 +0100

    Improve TableUtils test

--- tests/wesenGemaMod/Util/TestTableUtils.lua
+++ tests/wesenGemaMod/Util/TestTableUtils.lua
@@ -68,5 +68,115 @@ function TestTableUtils:canSliceTable(_startIndex, _endIndex, _expectedResult)
 
 end
 
+---
+-- Checks whether the inTable method works as expected.
+--
+function TestTableUtils:testCanDetectElementInTable()
+
+  local testValues = {
+
+    -- Characters
+    { { "a", "b", "c" }, "A", false },
+    { { "a", "b", "c" }, "a", true },
+    { { "a", "b", "c" }, "d", false },
+
+    -- Numbers
+    { { 1, 2, 3 }, 1, true },
+    { { 1, 2, 3 }, 0, false },
+    { { 1, 2, 3 }, 4, false }
+
+  };
+
+  for index, testValueSet in ipairs(testValues) do
+    self:canDetectElementInTable(unpack(testValueSet));
+  end
+
+end
+
+---
+-- Checks one of the data sets of testCanDetectElementInTable.
+--
+-- @tparam string[] _table The table
+-- @tparam string _element The element that shall be found in the table
+-- @tparam Bool _expectedReturnValue The expected return value
+--
+function TestTableUtils:canDetectElementInTable(_table, _element, _expectedReturnValue)
+
+  local result = TableUtils:inTable(_element, _table);
+  luaunit.assertEquals(result, _expectedReturnValue);
+
+end
+
+---
+-- Checks whether the tableSum method works as expected.
+--
+function TestTableUtils:testCanCalculateTableSum()
+
+  local testValues = {
+    { { 1, 2, 3 }, 6 },
+    { { 4, 3, 7 }, 14 },
+    { { 2, 8, 7 }, 17 },
+    { { 3, 4, 5, 6 }, 18 },
+    { { 1, 2 }, 3 }
+  }
+
+  for index, testValueSet in ipairs(testValues) do
+    self:canCalculateTableSum(unpack(testValueSet));
+  end
+
+end
+
+---
+-- Checks one of the data sets of testCanCalculateTableSum.
+--
+-- @tparam int[] _table The table of numbers
+-- @tparam int _expectedSum The expected table sum
+--
+function TestTableUtils:canCalculateTableSum(_table, _expectedSum)
+
+  local sum = TableUtils:tableSum(_table);
+  luaunit.assertEquals(sum, _expectedSum);
+
+end
+
+---
+-- Checks whether the copy method works as expected.
+--
+function TestTableUtils:testCanCopyTable()
+
+  local testTable = {
+    ["name"] = "gema",
+    ["rank"] = 1,
+    ["points"] = 1000000
+  }
+
+  -- Check that changing a reference also changes the source table
+  local testTableReference = testTable;
+  luaunit.assertEquals(testTable, testTableReference);
+  luaunit.assertEquals(tostring(testTable), tostring(testTableReference));
+
+  testTableReference["name"] = "notgema";
+  testTableReference["rank"] = 999;
+  testTableReference["points"] = 0;
+
+  luaunit.assertEquals(testTable["name"], "notgema");
+  luaunit.assertEquals(testTable["rank"], 999);
+  luaunit.assertEquals(testTable["points"], 0);
+
+  -- Check that changing a copied table does not change the source table
+  local testTableCopy = TableUtils:copy(testTable);
+  luaunit.assertEquals(testTable, testTableCopy);
+  luaunit.assertNotEquals(tostring(testTable), tostring(testTableCopy));
+
+  testTableCopy["name"] = "tdm";
+  testTableCopy["rank"] = 10000;
+  testTableCopy["points"] = -500;
+
+  luaunit.assertEquals(testTable["name"], "notgema");
+  luaunit.assertEquals(testTable["rank"], 999);
+  luaunit.assertEquals(testTable["points"], 0);
+
+end
+
 
 return TestTableUtils;
