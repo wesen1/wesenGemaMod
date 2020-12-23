98c766b340666cb3ab2d70ad4f1d8cf10c5732e6
wesen <wesen1@users.noreply.github.com>
2020-12-23 19:34:59 +0100

    Add UnplayableMapsRemover extension

--- src/wesenGemaMod/EventHandler/PlayerCallMapVoteHandler.lua
+++ src/wesenGemaMod/EventHandler/PlayerCallMapVoteHandler.lua
@@ -8,7 +8,6 @@
 local BaseEventHandler = require("EventHandler/BaseEventHandler");
 local Environment = require("EnvironmentHandler/Environment");
 local Exception = require("Util/Exception");
-local MapRemover = require("Map/MapRemover");
 local ObjectUtils = require("Util/ObjectUtils");
 
 ---
@@ -18,14 +17,6 @@ local ObjectUtils = require("Util/ObjectUtils");
 -- @type PlayerCallMapVoteHandler
 --
 local PlayerCallMapVoteHandler = setmetatable({}, {__index = BaseEventHandler});
-
-
----
--- The map remover
---
--- @tfield MapRemover mapRemover
---
-PlayerCallMapVoteHandler.mapRemover = nil;
 
 
 ---
@@ -40,8 +31,6 @@ function PlayerCallMapVoteHandler:__construct(_parentGemaMode)
   -- This is no lua server event, therefore no target event name is added to this event handler
   local instance = BaseEventHandler(_parentGemaMode);
   setmetatable(instance, {__index = PlayerCallMapVoteHandler});
-
-  instance.mapRemover = MapRemover();
 
   return instance;
 
@@ -67,9 +56,7 @@ function PlayerCallMapVoteHandler:handleEvent(_cn, _mapName, _gameMode, _minutes
 
   local player = self:getPlayerByCn(_cn)
 
-  if (_voteError == VOTEE_INVALID) then
-    return self:onInvalidMapVote(player, _mapName, _gameMode, _minutes)
-  elseif (_voteError == VOTEE_NOERROR) then
+  if (_voteError == VOTEE_NOERROR) then
     return self:onValidMapVote(player, _mapName, _gameMode, _minutes)
   end
 
@@ -77,50 +64,6 @@ end
 
 
 -- Private Methods
-
----
--- Handles invalid map votes.
---
--- @tparam Player _player The player that called the vote
--- @tparam string _mapName The map name
--- @tparam int _gameMode The game mode
--- @tparam int _minutes The number of minutes to load the map for
---
--- @treturn int|nil PLUGIN_BLOCK or nil
---
-function PlayerCallMapVoteHandler:onInvalidMapVote(_player, _mapName, _gameMode, _minutes)
-
-  if (mapexists(_mapName)) then
-    -- The map exists but the vote is invalid, this means that the map can not be loaded because it is unplayable
-
-    local status, exception = pcall(self.removeUnplayableMap, self, _mapName);
-    if (not status) then
-      if (ObjectUtils:isInstanceOf(exception, Exception)) then
-       self.output:printException(exception, _player);
-      else
-        error(exception);
-      end
-    end
-
-    return PLUGIN_BLOCK;
-  end
-
-end
-
----
--- Removes an unplayable map from the maprot and the maps folder.
---
--- @tparam string _mapName The map name
--- @tparam Player _player The player who tried to load the map
---
-function PlayerCallMapVoteHandler:removeUnplayableMap(_mapName, _player)
-
-  self.mapRemover:removeMap(_mapName, self.parentGemaMode:getMapRot());
-  self.output:printTextTemplate(
-    "TextTemplate/InfoMessages/Maps/AutomaticMapDeletion", { ["mapName"] = _mapName }, _player
-  )
-
-end
 
 ---
 -- Handles valid map votes.
