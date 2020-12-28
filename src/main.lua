2a4dad89fbc4de5c14016746d13a40b07d61c902
wesen <wesen1@users.noreply.github.com>
2020-12-28 05:08:44 +0100

    Adjust main.lua to use config file

--- src/main.lua
+++ src/main.lua
@@ -15,41 +15,25 @@ PLUGIN_VERSION = "0.1"
 --
 package.path = package.path .. ";lua/scripts/wesenGemaMod/?.lua"
 
-
 --
 -- Require the penlight compatibility module that adds some global functions that are missing in Lua5.1
 -- such as package.searchpath, table.unpack and table.pack
 --
 require "pl.compat"
 
+local config = require "lua.config.config"
+
+-- Wait for a specific time so that the database container has enough time to start
+local sleep = require "sleep"
+sleep(config.waitMillisecondsBeforeDatabaseConnection or 5000)
 
 -- Configure LuaORM
 local LuaORM_API = require "LuaORM.API"
-
--- Wait for 5 seconds so that the database container has enough time to start
-local sleep = require "sleep"
-sleep(5000)
-
-local config = cfg.totable("luaorm")
-LuaORM_API.ORM:initialize({
-  connection = "LuaSQL/MySQL",
-  database = {
-    databaseName = config.databaseName,
-    host = "database",
-    portNumber = 3306,
-    userName = config.databaseUser,
-    password = config.databasePassword
-  },
-  logger = { isEnabled = true, isDebugEnabled = false }
-})
+LuaORM_API.ORM:initialize(config.LuaORM or {})
 
 -- Configure AC-ClientOutput
 local ClientOutputFactory = require "AC-ClientOutput.ClientOutputFactory"
-local config = cfg.totable("gemamod")
-ClientOutputFactory.getInstance():configure({
-    fontConfigFileName = "FontDefault",
-    defaultConfiguration = config
-})
+ClientOutputFactory.getInstance():configure(config.ClientOutputFactory or {})
 
 -- Configure AC-LuaServer
 local Server = require "AC-LuaServer.Core.Server"
@@ -60,13 +44,12 @@ server:getPlayerList():setPlayerImplementationClass(Player)
 
 local ColorLoader = require "Config.ColorLoader"
 local TimeFormatter = require "Output.TimeFormatter"
-local colorConfigurationFileName = "colors"
 server:configure({
   Output = {
     TemplateRenderer = {
       StringRenderer = {
         defaultTemplateValues = {
-          colors = ColorLoader(colorConfigurationFileName):getColors(),
+          colors = ColorLoader(config.colorConfigurationFileName or "colors"):getColors(),
           timeFormatter = TimeFormatter()
         },
         templateBaseDirectoryPaths = {
@@ -78,46 +61,20 @@ server:configure({
 })
 
 -- Add the extensions to the server
-local AdditionalServerInfos = require "Extensions.AdditionalServerInfos"
-local AutoFlagReset = require "Extensions.AutoFlagReset"
-local CmdsCommand = require "Commands.CmdsCommand"
-local CommandManager = require "CommandManager.CommandManager"
-local ConnectionAmountLimiter = require "Extensions.ConnectionAmountLimiter"
-local ExtendTimeCommand = require "Commands.ExtendTimeCommand"
-local GemaGameMode = require "GemaMode"
-local GemaMapManager = require "Extensions.GemaMapManager"
-local GemaMapRotationManager = require "Extensions.GemaMapRotationManager.GemaMapRotationManager"
-local GameModeManager = require "AC-LuaServer.Extensions.GameModeManager.GameModeManager"
-local HelpCommand = require "Commands.HelpCommand"
-local MapStatisticsPrinter = require "Extensions.MapStatisticsPrinter"
-local MapTopCommand = require "Commands.MapTopCommand"
-local RulesCommand = require "Commands.RulesCommand"
-local ScoreAttemptManager = require "ScoreAttemptManager.ScoreAttemptManager"
-local ServerScoreManager = require "ServerScoreManager.ServerScoreManager"
-local ServerTopCommand = require "Commands.ServerTopCommand"
-local UnplayableGemaMapsRemover = require "Extensions.UnplayableGemaMapsRemover"
-
 local extensionManager = server:getExtensionManager()
 
--- Core extensions
-extensionManager:addExtension(GameModeManager())
-extensionManager:addExtension(GemaGameMode())
-extensionManager:addExtension(GemaMapManager())
-extensionManager:addExtension(MapStatisticsPrinter())
-extensionManager:addExtension(ScoreAttemptManager())
-extensionManager:addExtension(GemaMapRotationManager())
+local extensionPath, constructorParameters, extensionClass
+for key, value in pairs(config.extensions or {}) do
 
-extensionManager:addExtension(CommandManager())
-extensionManager:addExtension(CmdsCommand())
-extensionManager:addExtension(HelpCommand())
-extensionManager:addExtension(ExtendTimeCommand())
-extensionManager:addExtension(MapTopCommand())
-extensionManager:addExtension(RulesCommand())
-extensionManager:addExtension(ServerTopCommand())
+  if (type(key) == "string" and type(value) == "table") then
+    extensionPath = key
+    constructorParameters = value
+  else
+    extensionPath = value
+    constructorParameters = {}
+  end
 
--- Optional extensions
-extensionManager:addExtension(ServerScoreManager())
-extensionManager:addExtension(AutoFlagReset())
-extensionManager:addExtension(AdditionalServerInfos())
-extensionManager:addExtension(ConnectionAmountLimiter(2))
-extensionManager:addExtension(UnplayableGemaMapsRemover())
+  extensionClass = require(extensionPath)
+  extensionManager:addExtension(extensionClass(table.unpack(constructorParameters)))
+
+end
